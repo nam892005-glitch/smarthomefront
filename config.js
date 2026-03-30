1663c497c435bc009f53f837dc1ead14b85b3562
nam892005-glitch <nam892005@gmail.com>
2026-03-30 23:45:09 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-// ===== CONFIG API =====
-// đổi sang URL backend thực sự trên Render
-const API_BASE = "https://smarthome-github-io-7.onrender.com";
-const API = API_BASE;
-
-// helper cũ
-function api(path){
-  return API_BASE + path;
-}
-
-console.log("API:", API_BASE);
+const API = "https://smarthome-github-io-7.onrender.com";
